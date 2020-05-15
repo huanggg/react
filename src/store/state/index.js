d08a386cb613f6d92c9368f9d9d0ae0ac034b327
huanggg <292019984@qq.com>
2020-05-15 14:51:18 +0800

    Update index.js

--- src/store/state/index.js
+++ src/store/state/index.js
@@ -7,20 +7,4 @@ const initValue = {
   },
   conut:1
 };
-// const ADD_TO_CART = "ADD_TO_CART";
-// const reducer = function(state, action) {
-//   switch (action.type) {
-//     case ADD_TO_CART:     
-//       console.log(state.userInfo.name)
-//       return {
-//         userInfo: {
-//           name: action.userInfo
-//         },
-//         conut:2
-//       };
-//     default:
-//       return state;
-//   }
-// };
-
 export default createStore(reducer, initValue);
