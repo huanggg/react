f2f396aa99caa5e67e4164f1a8e297eef2d412a4
huanggg <292019984@qq.com>
2020-12-21 14:25:26 +0800

    Update index.js

--- src/store/state/index.js
+++ src/store/state/index.js
@@ -1,10 +1,30 @@
 import { createStore } from "redux";
 import reducer from "../reducer/index.js";
 
-const initValue = {
+const state = {
   userInfo: {
     name: "张三"
   },
   conut:1
 };
-export default createStore(reducer, initValue);
+
+// function reducer (state, action) => {
+//   switch (action.type) {
+//     case SET_USER_INFO :
+//       return {
+//         userInfo: {
+//           name: action.userInfo
+//         }
+//       };
+//      case SetAge:     
+//       return {        
+//         conut: action.Age
+//       };
+//     default:
+//       return state;
+//   }
+// };
+// reducer，reducer行为函数
+// preloadedState，初始化state数据
+// const store = createStore(reducer, [preloadedState], enhancer);
+export default createStore(reducer, state);
